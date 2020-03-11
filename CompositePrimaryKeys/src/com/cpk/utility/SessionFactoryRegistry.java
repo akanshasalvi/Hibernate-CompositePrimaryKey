a87a286cfb715eec16698cd393804a4f4e68713b
Admin <Admin@ADMINRG-PK096SO>
2020-03-12 00:02:38 +0530

    Created SessionFactory

---
+++ CompositePrimaryKeys/src/com/cpk/utility/SessionFactoryRegistry.java
@@ -0,0 +1,38 @@
+package com.cpk.utility;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class SessionFactoryRegistry {
+
+private static SessionFactory mysqlsessionFactory;
+	
+	static
+	{
+		try
+		{
+			mysqlsessionFactory=new MetadataSources(new StandardServiceRegistryBuilder().configure().build()).buildMetadata().buildSessionFactory();
+
+		}
+		catch (Throwable t)
+		{
+			t.printStackTrace();
+			throw t;
+		}
+	}
+
+	public static final SessionFactory getMysqlsessionFactory()
+	{
+		return mysqlsessionFactory;
+	}
+
+	public static void closesessionFactory()
+	{
+		if(mysqlsessionFactory!=null)
+		{
+			mysqlsessionFactory.close();
+		}
+	}
+
+}
